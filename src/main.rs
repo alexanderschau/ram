84b1b1c97247e439bf502e3afa7bffd28e75f703
Alexander Schau <alexander@schau.email>
2022-03-18 21:29:23 +0000

    start adding tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("Hello, world!");
+  let example_script = "📣 \"hello world\"
+    📣 \"from 🐏!\"";
+
+  println!("🐏 Ram Compiler");
+
+  tokenizer(example_script);
+}
+
+struct Token {
+  token_type: String,
+  value: String,
+}
+
+fn tokenizer(script: &str) {
+  let mut chars = script.chars();
+
+  let mut tokens: Vec<Token> = Vec::new();
+
+  while let Some(next_char) = chars.next() {
+    if next_char == '(' {
+      tokens.push(Token {
+        token_type: String::from("group"),
+        value: String::from("("),
+      })
+    }
+
+    if next_char == ')' {
+      tokens.push(Token {
+        token_type: String::from("group"),
+        value: String::from(")"),
+      })
+    }
+  }
 }
