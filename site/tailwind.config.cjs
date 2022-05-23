50616c8e32eac716d20356434f99689ea9bdbc9a
Alexander Schau <alexander@schau.email>
2022-05-23 16:53:12 +0000

    add first content

--- site/tailwind.config.cjs
+++ site/tailwind.config.cjs
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3C1EF4",
+        text: "#010F0A",
+      },
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
